2d7ad3f572eac35ae7b861bb7ad3083d81351729
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2024-01-03 10:36:19 +0800

    Update render.js

--- src/js/render.js
+++ src/js/render.js
@@ -1,8 +1,16 @@
+const {versions, ping, setTitle} = electronAPI
 const infoDom = document.querySelector('#info')
 infoDom.innerText = `本应用正在使用 Chrome (v${versions.chrome}), Node.js (v${versions.node}), 和 Electron (v${versions.electron})`
 
 const pingFunc = async ()=> {
-  const res = await versions.ping()
+  const res = await ping()
   infoDom.innerText += ' 接收到主应用消息：' + res.data
 }
 pingFunc()
+
+const setBtn = document.querySelector('#set-btn')
+const titleInput = document.querySelector('#title-txt')
+setBtn.addEventListener('click', ()=> {
+  const title = titleInput.value.trim()
+  if (title) setTitle(title)
+})
