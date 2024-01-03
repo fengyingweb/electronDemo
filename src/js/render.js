8780374e0be64139536233bf0cec5163a03e449c
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2024-01-03 11:07:25 +0800

    Update render.js

--- src/js/render.js
+++ src/js/render.js
@@ -1,4 +1,4 @@
-const {versions, ping, setTitle} = electronAPI
+const {versions, ping, setTitle, openFile} = electronAPI
 const infoDom = document.querySelector('#info')
 infoDom.innerText = `本应用正在使用 Chrome (v${versions.chrome}), Node.js (v${versions.node}), 和 Electron (v${versions.electron})`
 
@@ -13,4 +13,11 @@ const titleInput = document.querySelector('#title-txt')
 setBtn.addEventListener('click', ()=> {
   const title = titleInput.value.trim()
   if (title) setTitle(title)
+})
+
+const openBtn = document.querySelector('#open-btn')
+const filePathEle = document.querySelector('#filePath')
+openBtn.addEventListener('click', async ()=> {
+  const filePath = await openFile()
+  filePathEle.innerText = filePath
 })
